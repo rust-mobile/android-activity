a1ffb05cc8febd6a9c385d07ab096dbfb01b1e73
Robert Bragg <robert@sixbynine.org>
2022-10-20 20:52:14 +0100

    Adds a minimal na-winit-glutin example
    This example shows how to draw a triangle with GL[ES via the Glutin
    crate (the app works on desktop and Android) and on Android it
    demonstrates how to re-create the applications surface state when
    it is paused and resumed.
    
    The Renderer code and some utilities are from the upstream Glutin example.

---
+++ examples/na-winit-glutin/build.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::fs::File;
+use std::path::PathBuf;
+
+use cfg_aliases::cfg_aliases;
+use gl_generator::{Api, Fallbacks, Profile, Registry, StructGenerator};
+
+fn main() {
+    // XXX this is taken from glutin/build.rs.
+
+    // Setup alias to reduce `cfg` boilerplate.
+    cfg_aliases! {
+        // Systems.
+        android: { target_os = "android" },
+        wasm: { target_arch = "wasm32" },
+        macos: { target_os = "macos" },
+        ios: { target_os = "ios" },
+        apple: { any(target_os = "ios", target_os = "macos") },
+        free_unix: { all(unix, not(apple), not(android)) },
+
+        // Native displays.
+        x11_platform: { all(feature = "x11", free_unix, not(wasm)) },
+        wayland_platform: { all(feature = "wayland", free_unix, not(wasm)) },
+
+        // Backends.
+        egl_backend: { all(feature = "egl", any(windows, unix), not(apple), not(wasm)) },
+        glx_backend: { all(feature = "glx", x11_platform, not(wasm)) },
+        wgl_backend: { all(feature = "wgl", windows, not(wasm)) },
+        cgl_backend: { all(macos, not(wasm)) },
+    }
+
+    let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
+    let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
+    Registry::new(Api::Gles2, (3, 0), Profile::Core, Fallbacks::All, [])
+        .write_bindings(StructGenerator, &mut file)
+        .unwrap();
+}
