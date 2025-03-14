49f2b864247c887b4f894ecef1372e74c5975ff6
William Casarin <jb55@jb55.com>
2025-03-13 17:28:19 -0700

    switch to android-game-sdk-rs grafted repo at v2.0.2
    Also includes our patches on top (branch android-activity-2.0.2). This
    is mainly to test to make sure everything is still working. We will
    switch to the android-activity-4.0.0 branch when we're done
    
    Signed-off-by: William Casarin <jb55@jb55.com>

---
+++ android-activity/android-games-sdk/README.md
@@ -0,0 +1,40 @@
+# android-games-sdk-rs
+
+This is a grafted version of [android-games-sdk-rs][agdk].
+
+The purpose of android-games-sdk-rs is to track our patches on top of the Android Game Development Kit (AGDK) so that it is easier to update to new upstream versions.
+
+## Updating to new agdk version checklist
+
+This is a basic checklist for things that need to be done before updating to a new agdk version:
+
+- [ ] Ensure all patches applied to the local repo have been backported (see section below on backporting patches)
+- [ ] Rebase patches on top of a new *release* version of agdk. If you're not sure which version you should be rebasing on, open an issue.
+- [ ] Copy any new files over from the new version using the [copy_files](./copy_files) script (see section below on copying files)
+- [ ] Update [build.rs](../build.rs) with any new includes and src files
+- [ ] Regenerate ffi bindings using [generate_bindings.sh](./generate_bindings.sh)
+
+## Backporting patches
+
+Changes made to these files must be backported to [android-games-sdk-rs][agdk], otherwise they will be lost when updating to newer upstream versions. This can be done like so (running from the project root):
+
+```bash
+git format-patch -o ~/agdk-patches last-import.. -- android-activity/android-games-sdk
+```
+
+When applying on the [android-games-sdk-rs][agdk] side:
+
+```bash
+git checkout android-activity-4.0.0
+git am -p3 ~/agdk-patches/*.patch
+```
+
+Once these are applied on top of the current imported agdk version (in this example 4.0.0). Then they can be rebased onto future agdk versions easily.
+
+## Copying files from agdk
+
+When updating to a new version of upstream agdk properly, you can use the [copy_files](./copy_files) script.
+
+This script takes in a list of files from [file_list.txt](./file_list.txt), and grafts those files into the local directory. This ensures that pathes are aligned so that patching is easier.
+
+[agdk]: https://github.com/rust-mobile/android-games-sdk-rs
--- android-activity/build.rs
+++ android-activity/build.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)]
 
 fn build_glue_for_game_activity() {
+    let activity_basepath = "android-games-sdk/game-activity/prefab-src/modules/game-activity/include";
+    let textinput_basepath = "android-games-sdk/game-text-input/prefab-src/modules/game-text-input/include";
+
     for f in [
         "GameActivity.h",
         "GameActivity.cpp",
@@ -8,34 +11,40 @@ fn build_glue_for_game_activity() {
         "GameActivityEvents.cpp",
         "GameActivityLog.h",
     ] {
-        println!("cargo:rerun-if-changed=game-activity-csrc/game-activity/{f}");
+        println!("cargo:rerun-if-changed={activity_basepath}/game-activity/{f}");
     }
     cc::Build::new()
         .cpp(true)
-        .include("game-activity-csrc")
-        .file("game-activity-csrc/game-activity/GameActivity.cpp")
-        .file("game-activity-csrc/game-activity/GameActivityEvents.cpp")
+        .include("android-games-sdk/include")
+        .include(activity_basepath)
+        .include(textinput_basepath)
+        .file(format!("{activity_basepath}/game-activity/GameActivity.cpp"))
+        .file(format!("{activity_basepath}/game-activity/GameActivityEvents.cpp"))
         .extra_warnings(false)
         .cpp_link_stdlib("c++_static")
         .compile("libgame_activity.a");
 
     for f in ["gamecommon.h", "gametextinput.h", "gametextinput.cpp"] {
-        println!("cargo:rerun-if-changed=game-activity-csrc/game-text-input/{f}");
+        println!("cargo:rerun-if-changed={textinput_basepath}/game-text-input/{f}");
     }
     cc::Build::new()
         .cpp(true)
-        .include("game-activity-csrc")
-        .file("game-activity-csrc/game-text-input/gametextinput.cpp")
+        .include("android-games-sdk/include")
+        .include(textinput_basepath)
+        .file(format!("{textinput_basepath}/game-text-input/gametextinput.cpp"))
         .cpp_link_stdlib("c++_static")
         .compile("libgame_text_input.a");
 
     for f in ["android_native_app_glue.h", "android_native_app_glue.c"] {
-        println!("cargo:rerun-if-changed=game-activity-csrc/native_app_glue/{f}");
+        println!("cargo:rerun-if-changed={activity_basepath}/game-activity/native_app_glue/{f}");
     }
+    
     cc::Build::new()
-        .include("game-activity-csrc")
-        .include("game-activity-csrc/game-activity/native_app_glue")
-        .file("game-activity-csrc/game-activity/native_app_glue/android_native_app_glue.c")
+        .include("android-games-sdk/include")
+        .include(activity_basepath)
+        .include(textinput_basepath)
+        .include(format!("{activity_basepath}/game-activity/native_app_glue"))
+        .file(format!("{activity_basepath}/game-activity/native_app_glue/android_native_app_glue.c"))
         .extra_warnings(false)
         .cpp_link_stdlib("c++_static")
         .compile("libnative_app_glue.a");
